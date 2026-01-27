9a8f2672afa7dc14699ed3b5c13b8f50f82dacfd
anurag <as0326835@gmail.com>
2026-01-27 13:57:54 +0530

    flask

--- Flask_demo/flask_demo_1.py
+++ Flask_demo/flask_demo_1.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Flask!"
+    return "Hello Flask World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
